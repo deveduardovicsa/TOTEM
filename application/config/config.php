0f5116e3f292ecb0b16d75599180ec4859d4dbb9
deveduardovicsa <142454584+deveduardovicsa@users.noreply.github.com>
2023-08-18 10:29:18 -0400

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/totem/';
-
+/*ya po mati, siempre pasa algo!!!*/
 /*
 |--------------------------------------------------------------------------
 | Index File
